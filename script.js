74e088599e849de72621111b336742c13e008ce1
Bhavana Singh <100597557+Bhavana-Somvanshi@users.noreply.github.com>
2023-09-11 18:38:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let display = document.getElementById('display');
+let currentInput = '';
+
+function appendToDisplay(value) {
+    currentInput += value;
+    display.value = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = '';
+    display.value = '';
+}
+
+function calculateResult() {
+    try {
+        let result = eval(currentInput);
+        display.value = result;
+        currentInput = result.toString();
+    } catch (error) {
+        display.value = 'Error';
+        currentInput = '';
+    }
+}
